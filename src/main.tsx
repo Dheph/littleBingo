27ec51ea4607e985734f85445c1e78c33634a9cb
Dheph <dhepherson.miranda@outlook.com>
2026-05-20 18:48:45 -0300

    fix: use official Chakra UI v3 setup with defaultSystem + next-themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { ThemeProvider } from 'next-themes'
 import { HashRouter } from 'react-router-dom'
-import { system } from './theme'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider value={system}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </ThemeProvider>
     </ChakraProvider>
   </StrictMode>,
 )
